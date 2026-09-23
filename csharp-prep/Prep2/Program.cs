30b8dcf118fcffc674d9b08c3e3f0231a908488d
rebeljakefang <rebeljake16.jl@gmail.com>
2026-09-22 21:21:59 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,7 +11,37 @@ class Program
         Console.Write("what is your grade ");
         string userInput = Console.ReadLine();
         int number = int.Parse(userInput);
-        if (userInput = 100);
+        string letter = "";
+        
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"your grade is {letter}");
 
+        if (number >= 70)
+        {
+            Console.WriteLine("you passed!");
+        }
+        else
+        {
+            Console.WriteLine("you did not pass");
+        }
     }
 }
